c9f5ec1b7441097cbc3be48b855ce8be26be2189
XEternalKingX <zoeyac02@gmail.com>
2026-02-13 00:28:30 -0600

    Player Animation Testing

---
+++ A RAY(2D)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    public Animator anim;
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        anim.SetFloat("horizontalInput", Mathf.Abs(horizontalInput));
+ 
+        //Flip player when facing left/right.
+        if (horizontalInput > -0.01f)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        else if (horizontalInput < 0.01f)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+         if (Input.GetKey(KeyCode.Space))
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        
+    }
+}
