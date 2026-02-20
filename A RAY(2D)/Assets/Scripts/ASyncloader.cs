914742d165de8f0028ba22c8e8554d4308e83e30
Yosselin <yosselinmeza993@gmail.com>
2026-02-20 12:41:53 -0600

    Updated Main Menu, removed More & Settings scenes, added AsyncLoader

---
+++ A RAY(2D)/Assets/Scripts/ASyncloader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ASnycloader : MonoBehaviour
+{
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject MainMenu;
+
+    [Header("Slider")]
+    [SerializeField] private Slider loadingSlider;
+    public void LoadLevelBtn(string levelToLoad)
+    {
+        MainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelASync(levelToLoad));
+    }
+    IEnumerator LoadLevelASync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while (!loadOperation.isDone)
+        {
+        float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+        loadingSlider.value = progressValue;
+        yield return null;
+        }
+    }
+}
--- A RAY(2D)/Assets/Scripts/MainMenuController.cs
+++ A RAY(2D)/Assets/Scripts/MainMenuController.cs
@@ -3,19 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("PrototypeScene");
-    }
-
-    public void OpenMore()
-    {
-        SceneManager.LoadScene("More Menu");
-    }
-
-    public void OpenSettings()
-    {
-        SceneManager.LoadScene("Settings Menu");
+    public void Exit(){
+        SceneManager.LoadScene("Main Menu");
     }
    
 }
