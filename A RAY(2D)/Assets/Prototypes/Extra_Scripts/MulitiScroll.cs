f425c8fda0f5d1a8e28aae38fe25261536b6797e
S3aFl3a <marleyOREO2002@gmail.com>
2026-03-05 18:47:44 -0600

    Added Randomizer to Background in Main Menu
    Changed coloring to Main Menu, can change later, still deciding color. Added backgrounds + script to main menu that randomizers it

---
+++ A RAY(2D)/Assets/Prototypes/Extra_Scripts/MulitiScroll.cs
@@ -0,0 +1,56 @@
+using UnityEngine; // Required for all Unity based Code
+using UnityEngine.UI;
+
+
+public class MulitiScroll : MonoBehaviour //this will be the behavior
+
+{
+    //Determines the speed of scrolling (Change higher if faster speed needed)
+    public float scrollSpeed = 0.3f;
+    // Background needs to be RAWIMAGE
+    public Texture[] backgrounds; //Needed for my list of backgrounds
+
+    private RawImage rawImage;
+
+    private float offset; //According to unity's references, keep tracks of amount (in this case scrolling)
+    private int lastIndex = -1;
+
+    void Start()
+    {
+        rawImage = GetComponent<RawImage>(); //Will get rawimage to this object
+
+        ChangeBackground();
+    }
+
+    void Update()
+    {
+        float previousOffset = offset;
+        // The effect of .scrolling
+        // As shown in to visual studio, the more it scrolls, it increases offset overtime
+        offset += scrollSpeed * Time.deltaTime;
+
+        offset = Mathf.Repeat(offset, 1f);
+        if (offset < previousOffset)
+        {
+           
+            ChangeBackground();
+        }
+        rawImage.uvRect = new Rect(offset, 0f, 1f, 1f);
+
+      
+    }
+
+    void ChangeBackground()
+    {
+        if (backgrounds.Length == 0) return;
+        int newIndex;
+        do
+        {
+            newIndex = Random.Range(0, backgrounds.Length);
+        }
+        while (newIndex == lastIndex && backgrounds.Length > 1);
+
+        lastIndex = newIndex;
+        rawImage.texture = backgrounds[newIndex];
+    }
+}
