720ccff672f5e92a62a5036c8a91d170ccb0f18f
Yosselin <yosselinmeza993@gmail.com>
2026-04-30 19:29:46 -0500

    adding on to the hashmap code

--- A RAY(2D)/Assets/Scripts/Hashmap.cs
+++ A RAY(2D)/Assets/Scripts/Hashmap.cs
@@ -7,20 +7,27 @@ public class Hashmap : MonoBehaviour
 {
     //Breakdown of how the syntax works:
     //Dictionary -> this is the data structure that stores data in pairs. In our case the Key and value
-    //<string, GameObject> -> This is the Key and value. The key will be storing a data type of string. And value is what you get back from that string.
+    //<string, GameObject> -> This is the Key and value. The key will be storing a data type of string. While the value will store whatever the the key gives (in our case a prefab).
     //Example: key -> 'rock' and value -> The prefab that belongs to 'rock'
     //BG -> This is the name of the dictionary. This name will be used to add data later to the dictionary
     //new  Dictionary<string, Sprite>() -> this will make a new empty dictionary that will store a string and the object(value) that belongs to the key
     //() -> actually runs the building process 
     Dictionary<string, GameObject> BG = new  Dictionary<string, GameObject>();
     private GameObject background; //this will be used to call the current background being displayed in gamescene 
+    public GameObject defaultBackground; //this is used in Start()
 
-
-    void start()
+    //loads all the prefabs and the default background at the start of the game
+    void Start()
     {
+        //this will load all the backgrounds into our Dictionary(BG)
         prefabBG();
+        //This is storing the default background. So it will go into Resources and look for the prefab that name starts with default.
+        //then it will save it in defaultBackground to use later in the code.
+        defaultBackground = Resources.Load<GameObject>("Prefabs/default");
     }
 
+
+    //function purpose: populates the dictionary with the key and value of each prefab located in 'Assets/Resources/Prefabs'
     void prefabBG()
     {
         //This holds all the prefabs located in 'Assets/Resources/Prefabs'
@@ -49,26 +56,58 @@ public class Hashmap : MonoBehaviour
                BG.Add(key, Oneprefab); 
                Debug.Log("Adding prefab key name:" + key); //Just a debuger
             }
-
-
         }
     }
 
 
-        //still building... 
+      //function purpose: This will select and apply the background based on the genre given by the API, but if the API doesn't return aything default background will display
     public void ChangeBackground(string genre)
     {
+        //checking if the genre is return empty or unknown by the API
         if (string.IsNullOrEmpty(genre))
         {
-            Debug.Log("Genre was not received and is unknown");
+            //if the genre is empty or unknown set the game scene as the defaultBackground (forest world)
+            SetBackground(defaultBackground);
+            Debug.Log("Genre was not received and is unknown or empty"); //just a debuger to let us know why the background didn't change
             return;
         }
 
+        //converting the letters in genre as lowercase to match the 'key' names
+        //example: if the genre gets return as 'Rock' it will convert to 'rock'
+        genre = genre.ToLower();
 
-        
+        //looping through all the prefabs in our dictionary(BG)
+        //what is 'prefab' -> all the existing prefabs that now have a key and a value in our dictionary
+        //remember Key is the string -> in our case the genre name of the prefab (example: 'rock')
+        //value is the prefab itself that belongs to the key
+        foreach (var prefab in BG)
+        {
+            //if the genre given by the api matchs one of the 'prefab' key 
+            if (genre.Contains(prefab.Key))
+            {
+                //then set then key's own value to the game scene.
+                SetBackground(prefab.Value);
+                Debug.Log("The genre matches the key name called: " + prefab.Key + "and set the background to its value"); //just a debuger to see the change worked in console
+                return;
+
+            }
+    
+        }
     }
 
-   
+    //function purpose: its job it to destroy(remove) the current background and replace it with the next prefab's backgrounds 
+    void SetBackground(GameObject prefab)
+    {
+        //checking if there already a existing background in the game scene
+        if (background != null)
+        {
+            //we are destroying the old background at runtime (aka removing not actually destorying the original prefab)
+            //just the copy the prefab creates in the gamescene
+            Destroy(background);
+        }
+        //Then creating a new instant of the new prefab's background in the gamescene that replaces the old prefab's background
+        background = Instantiate(prefab);
+    }
 
 
 }
